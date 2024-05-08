18a54cffb870de48ac03f58d30d9f1d28425e067
Roberta Santana <g.roberta.s@hotmail.com>
2024-05-08 19:04:04 -0300

    Onclick no JS

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-document.querySelector('.tecla_pow');{
-    return 
+function tocarSomPom(){
+    document.querySelector('#som_tecla_pom').play();
 }
+
+/*EXECUTANDO SEM A FUNÇÃO ONCLICK NO HTML*/
+function tocarSomClap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick = tocarSomClap;
