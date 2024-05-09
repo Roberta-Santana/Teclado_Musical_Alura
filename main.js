28c804114531b3b4080f6b13f1763ae32861b7d5
Roberta Santana <g.roberta.s@hotmail.com>
2024-05-09 11:44:02 -0300

    interando em Lista

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 function tocarSomPom(){
     document.querySelector('#som_tecla_pom').play();
-}
+}/*POR ALGUMA RAZÃO QUE EU DESCONHEÇO, A FUNÇÃO tocarSomPom vai sem os ()
+sem parênteses para garantir que a função seja chamada dinamicamente 
+no momento do evento (clique), e não imediatamente durante a atribuição.
+*/
 
-/*EXECUTANDO SEM A FUNÇÃO ONCLICK NO HTML*/
+/*EXECUTANDO SEM A FUNÇÃO ONCLICK NO HTML
 function tocarSomClap(){
     document.querySelector('#som_tecla_clap').play();
 }
-document.querySelector('.tecla_clap').onclick = tocarSomClap;
+document.querySelector('.tecla_clap').onclick = tocarSomClap; */
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+/* const listaDeSons = document.querySelector('#som');*/
+let contador = 0;
+
+while(contador<listaDeTeclas.length){
+    listaDeTeclas[contador].onclick=tocarSomPom;
+    contador++;
+    console.log(contador);
+}
+
+
+
