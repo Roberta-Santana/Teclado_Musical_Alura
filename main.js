435db30e0f01e8409d1b2e33cd4ee7531e413268
Roberta Santana <g.roberta.s@hotmail.com>
2024-05-17 18:35:03 -0300

    aplicando o for

--- main.js
+++ main.js
@@ -16,27 +16,27 @@ function tocaSom(idElementoAudio){
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
+//let contador = 0;
 
-while(contador<listaDeTeclas.length){
+/* while(contador<listaDeTeclas.length){
 
         // 1º CRIANDO LISTA DE BOTÕES/ LIGANDO BOTÃO A INDICE DE LISTA 
     const tecla =listaDeTeclas[contador];
-    /*  listaDeTeclas = lista de todos os elementos button da classe tecla;
+    ->  listaDeTeclas = lista de todos os elementos button da classe tecla;
         -> While: Cada loop associa o indice do loop a um botão/tecla específico
         listaDeTeclas[0=button.tecla.tecla.pom;1=button.tecla.tecla.clap;2=button.tecla.tecla.tim...]
         tecla =  listaDeTeclas[contador];
-    */ console.log(tecla);
+    console.log(tecla);
 
 
         //2º LISTA QUE ASSOCIA O INDICE DA LISTA TECLAS/BOTÃO A UMA CLASSE ESPECÍFICA (QUE REPETE NO ID DO AUDIO) 
     const instrumento = tecla.classList[1];
-    /*  instrumento = a tecla/button.tecla.tecla.som com referencia por indice +
-    classlist = lista das classe daquele elemento 0=tecla;1=tecla_som,
-    tecla.classList[1] = usando indice 1, que repete no id do som
-    Intrumento é uma lista, que associa o indice da lista tecla, a uma classe espefica tecla_som
-    instrumento
-    */ console.log(instrumento);
+    -> instrumento = a tecla/button.tecla.tecla.som com referencia por indice +
+        classlist = lista das classe daquele elemento 0=tecla;1=tecla_som,
+        tecla.classList[1] = usando indice 1, que repete no id do som
+        Intrumento é uma lista, que associa o indice da lista tecla, a uma classe espefica tecla_som
+        instrumento
+     console.log(instrumento);
 
 
         //3º LISTA QUE ASSOCIA O ID DO SOM E A CLASSE DA LISTA INSTRUMENTO #som_${instrumento} CRASE
@@ -51,7 +51,14 @@ while(contador<listaDeTeclas.length){
 
     contador++;
     console.log(contador);
+}*/
+
+for(contador=0;contador<listaDeTeclas.length;contador++){
+const tecla =listaDeTeclas[contador];
+const instrumento = tecla.classList[1];
+const IdAudio = `#som_${instrumento}`;
+
+tecla.onclick = function(){
+    tocaSom(IdAudio);
 }
-
-
-
+}
